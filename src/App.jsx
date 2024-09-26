159a881badaa360ffda4e351c3a1c5de4d3a93e4
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-09-26 19:01:58 +0530

    Complete setup of store and  initialize App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux';
+import authService from './appwrite/auth';
+import {login, logout} from './store/authSlice';
 import './App.css'
+import { Footer, Header } from './components';
 
 function App() {
 
   /* this(console one) is basic way to access the environment variables created in vite app
    second way used in production grade apps is to create a conf file in which all the env variables are 
    exported the below way is not efficient cause sometimes it can misread string type which causes huge error 
-  console.log(import.meta.env.VITE_APPWRITE_URL); */
+  ---- console.log(import.meta.env.VITE_APPWRITE_URL); */
   
-  return (
-    <>
-      <h1 className='text-red-500 bg-green-600'>This is a mega project of blog website</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className="flex flex-wrap content-between min-h-screen bg-gray-500">
+      <div className='block w-full'>
+        <Header />
+        <main>
+          { /* <Outlet /> */ }
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
